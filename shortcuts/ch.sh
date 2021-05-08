b2389024d1994e0dd3c2833bbc9fedfc78829a4c
yashraj <yashraj11rathi@gmail.com>
2021-05-08 12:37:10 +0530

    Complete directory manager to move through them

--- shortcuts/ch.sh
+++ shortcuts/ch.sh
@@ -1,43 +1,72 @@
 #!/bin/sh
 
+#Directory manager for linux so that one can transerve through directories with ease
+
 case $1 in
 	-a)
 		loc=$(pwd)
 		echo $loc
-		res=$(grep -f ~/shellscripts/shortcuts/dir_list.csv $loc)
-	;;
-	*)
-	if	
-		test -z $1
-	then
 
-		echo false path 
 		while IFS=, read -r code dr;
 		do
-		
-			echo -e $code '\t' $dr
-		
+			if
+				test $loc == $dr
+			then
+				echo This directory is already present in my log
+				return;
+			fi
+
 		done < ~/shellscripts/shortcuts/dir_list.csv
-		
-		read option
 
-	else
+		lines=$(wc -l ~/shellscripts/shortcuts/dir_list.csv |cut -d' ' -f1)
+		lines=$((lines+1))
+		echo Code for this dir is $lines
+		echo $lines,$(pwd) >> ~/shellscripts/shortcuts/dir_list.csv
 
-		option=$1;
-		echo true path 
+		return;
+	;;
 
-	fi
-
-	while IFS=, read -r code dr;
-	do
-		echo Checking for $code
-		if
-			test $option -eq $code
+	*)
+		if 
+			test -f ~/shellscripts/shortcuts/dir_list.csv; 
 		then
-			cd $dr
+			:
+		else
+			echo No previous directory logs exists'!!!'
 			return;
 		fi
 
-	done < ~/shellscripts/shortcuts/dir_list.csv
+		if	
+			test -z $1
+		then
+
+			while IFS=, read -r code dr;
+			do		
+				echo -e $code '\t' $dr
+			done < ~/shellscripts/shortcuts/dir_list.csv
+			
+			echo -n Enter a valid option : 
+			read option
+			
+		else
+			
+			option=$1;
+			
+		fi
+
+		while IFS=, read -r code dr;
+		do
+			if
+				test $option -eq $code
+			then
+				cd $dr
+				return;
+			fi
+		
+		done < ~/shellscripts/shortcuts/dir_list.csv
+		
+		echo Directory not changed.
+		echo Please enter a valid option here.
+		
 	;;
 esac
