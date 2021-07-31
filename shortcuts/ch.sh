4d1b7b9443076883a33b00b3489f07c3cf1dbbc7
Yashraj Rajesh Rathi <ryashraj@athenahealth.com>
2021-07-31 23:53:27 +0530

    Added a delete option such that one can delete some paths from my saved file

--- shortcuts/ch.sh
+++ shortcuts/ch.sh
@@ -3,6 +3,20 @@
 #Directory manager for linux so that one can transerve through directories with ease
 #Normally very boring to remember your previously logged directory so this is much better
 
+re_eval()
+{
+
+	line_no=1;
+	while IFS=, read -r code dr;
+	do
+		echo $line_no,$dr >> ~/shellscripts/shortcuts/dir_list_temp.csv
+		line_no=$((line_no+1))
+
+	done < ~/shellscripts/shortcuts/dir_list.csv
+
+	cat ~/shellscripts/shortcuts/dir_list_temp.csv > ~/shellscripts/shortcuts/dir_list.csv
+	rm ~/shellscripts/shortcuts/dir_list_temp.csv
+}
 
 case $1 in
 	-a)
@@ -20,11 +34,32 @@ case $1 in
 
 		done < ~/shellscripts/shortcuts/dir_list.csv
 
-		lines=$(wc -l ~/shellscripts/shortcuts/dir_list.csv |cut -d' ' -f1)
+		lines=$(wc -l ~/shellscripts/shortcuts/dir_list.csv | sed 's/^ *//' | cut -d' ' -f1)
 		lines=$((lines+1))
 		echo Code for this dir is $lines
 		echo $lines,$(pwd) >> ~/shellscripts/shortcuts/dir_list.csv
 
+		return;
+	;;
+
+	-d)
+		while IFS=, read -r code dr;
+		do		
+			echo -e $code '\t' $dr
+		done < ~/shellscripts/shortcuts/dir_list.csv
+
+		read code_of_directory_to_be_deleted;
+
+		while IFS=, read -r code dr;
+		do
+			if
+				test $code_of_directory_to_be_deleted -eq $code
+			then
+				sed -i.csv -e $code_of_directory_to_be_deleted'd' ~/shellscripts/shortcuts/dir_list.csv
+			fi
+		
+		done < ~/shellscripts/shortcuts/dir_list.csv
+		re_eval
 		return;
 	;;
 
@@ -61,7 +96,7 @@ case $1 in
 			if
 				test $option -eq $code
 			then
-				cd $dr
+				cd "$dr"
 				return;
 			fi
 		
