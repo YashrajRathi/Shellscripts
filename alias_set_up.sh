bfcb95553fe2e146f148a40f8ec195020511d812
Yashraj Rajesh Rathi <ryashraj@athenahealth.com>
2021-08-25 23:07:33 +0530

    Added alias set up script in this repo to easily set aliases

---
+++ alias_set_up.sh
@@ -0,0 +1,8 @@
+alias jj='~/shellscripts/shortcuts/jj.sh'
+alias hm='cd ~/shellscripts/shortcuts'
+alias gg='~/shellscripts/shortcuts/gg.sh'
+alias ch='source ~/shellscripts/shortcuts/ch.sh'
+alias aa='source  ~/shellscripts/shortcuts/ch.sh -a'
+alias sudo='sudo ' ## In order to gedit with root access
+alias sk='~/shellscripts/shortcuts/sk.sh'
+alias chd='source ~/shellscripts/shortcuts/ch.sh -d'
